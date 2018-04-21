3e7db15f8f8b2e411a6a78d16b68071516ac4a1a
belloq <unlimitedstress@gmail.com>
2018-04-21 11:12:29 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -23,10 +23,10 @@ defmodule CheckoutElixir.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 0.13"},
+      {:httpoison, "~> 1.0"},
       {:poison, "~> 3.1"},
       {:ex_doc, "~> 0.14", only: :dev},
-      {:exvcr, "~> 0.9", only: :test}
+      {:exvcr, "~> 0.10", only: :test}
     ]
   end
 
