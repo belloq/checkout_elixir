1046cb10b06205a7189d3fd350a49133427d286d
belloq <unlimitedstress@gmail.com>
2020-03-26 14:43:09 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CheckoutElixir.Mixfile do
   def project do
     [
       app: :checkout_elixir,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
