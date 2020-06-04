0aba397b6a9d6e254b842fa6f3ba8bf07bd2ee4c
ADJ89 <arno.de.jonge@verycreatives.com>
2020-06-04 11:42:14 +0200

    update doc examples for payment#create

--- lib/checkout/payment.ex
+++ lib/checkout/payment.ex
@@ -16,25 +16,22 @@ defmodule Checkout.Payment do
       id: "src_en2w67spfevehol7rh6m5zokeq",
       cvv: "100",
     },
-    email: "test@example.org",
     amount: 6500,
     currency: "USD",
-    reference: "ORD-5023-4E89"
   })
   ```
   Create a Payment with Card Token.
 
   ## Example
   ```
-    Checkout.Payment.create(%{
-      source: %{
-        type: "token",
-        token: "card_tok_9EDE49...A52CC25"
-      },
-      amount: 2000,
-      currency: "USD",
-      reference: "TRK12345"
-    })
+  Checkout.Payment.create(%{
+    source: %{
+      type: "token",
+      token: "tok_wpzi5ph5r34uxfgvmxda67myva"
+    },
+    amount: 100,
+    currency: "USD",
+  })
   ```
   Create a Payment with Full Card.
 
